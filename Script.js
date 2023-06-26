dddf468f247750b95a0f8af49e4799c4c24b805e
Shivam Malhotra <malhotrashivam77@gmail.com>
2023-06-26 21:29:45 +0530

    update

--- Script.js
+++ Script.js
@@ -37,18 +37,26 @@ function toggleContent() {
     }
 }
 
-// -----submit----
-
+const scriptURL = 'https://script.google.com/macros/s/AKfycby-Ivfp1uj7-dPd0NuhqZXSzSteNuEd05kFiJET4lWFtU83HUYDSlCa5a50DWLy6Dgk/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then(response => {
-        msg.innerHTML = "Message sent successfully";
-        setTimeout(function () {
-          msg.innerHTML = "";
-        }, 1000);
-        form.reset();
-      })
-      .catch(error => console.error('Error!', error.message));
-  });
+  e.preventDefault();
+  form.classList.add("loading"); // Add loading class to show loading state
+  msg.innerHTML = ""; // Clear previous message
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
+      form.classList.remove("loading"); // Remove loading class
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 1000);
+      form.reset(); // Reset the form
+    })
+    .catch(error => {
+      form.classList.remove("loading"); // Remove loading class
+      console.error('Error!', error.message);
+    });
+});
